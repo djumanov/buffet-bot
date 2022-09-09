7a27d5e694be1a657fe9c41a21bc63abd9d62c06
djumanov <diyorbekjumanov1120@gmail.com>
2022-09-09 22:35:51 +0500

    main func

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-from telegram.ext import Updater, CommandHandler, CallbackContext
-from telegram import Update, constants
+from telegram.ext import Updater, CommandHandler
 import os
-
-TOKEN = os.environ['TOKEN']
-
-updater = Updater(token=TOKEN)
-dispatcher = updater.dispatcher
+from commands import start
 
 
-def start(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /start is issued."""
-    user = update.message.from_user.full_name
-    update.message.reply_text(text=f'Hello {user}! \nWelcome to our buffet bot.')
+def main() -> None:
+    """Start the bot."""
+    TOKEN = os.environ['TOKEN']
+
+    updater = Updater(token=TOKEN)
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(handler=CommandHandler(command='start', callback=start))
+
+    updater.start_polling()
+    updater.idle()
 
 
-dispatcher.add_handler(handler=CommandHandler(command='start', callback=start))
-
-updater.start_polling()
-updater.idle()
+if __name__ == "__main__":
+    main()
